ebebd0f9aef2bc238e173a7b0fdf599588ad9cc2
CakesTwix <oleg.kiryazov@gmail.com>
2024-05-04 20:23:39 +0300

    python: Add check for empty {}

--- copy2assets.py
+++ copy2assets.py
@@ -1,21 +1,20 @@
 import os
 import shutil
 
-directory = 'vs'
+directory = 'uk'
 
 
 for filename in os.listdir(directory):
     if filename.endswith(".json"):
         with open(os.path.join(directory, filename), 'r') as file:
-            if file.read() == "{}":
+            if "{}" in file.read():
                 print(f"Skipping file {filename}")
-                continue
+            else:
+                modname = filename.split("-")[0]
+                # assets/modname/lang/uk.json
+                new_path = os.path.join('assets', modname, 'lang', 'uk.json')
 
-        modname = filename.split("-")[0]
-        # assets/modname/lang/uk.json
-        new_path = os.path.join('assets', modname, 'lang', 'uk.json')
+                os.makedirs(f"assets/{modname}/lang", exist_ok=True)
+                shutil.copy(os.path.join(directory, filename), new_path)
 
-        os.makedirs(f"assets/{modname}/lang", exist_ok=True)
-        shutil.move(os.path.join(directory, filename), new_path)
-
-        print(f"File {filename} to {new_path}")
+                print(f"File {filename} to {new_path}")
