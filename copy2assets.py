77ce9119b1565a0f72534a22099ac42fe2b48e72
CakesTwix <oleg.kiryazov@gmail.com>
2024-05-04 19:57:03 +0300

    crowdin: Global sync

--- copy2assets.py
+++ copy2assets.py
@@ -6,6 +6,11 @@ directory = 'vs'
 
 for filename in os.listdir(directory):
     if filename.endswith(".json"):
+        with open(os.path.join(directory, filename), 'r') as file:
+            if file.read() == "{}":
+                print(f"Skipping file {filename}")
+                continue
+
         modname = filename.split("-")[0]
         # assets/modname/lang/uk.json
         new_path = os.path.join('assets', modname, 'lang', 'uk.json')
